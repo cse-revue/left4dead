c779b077b80cd885ce0367a06e6adaf4662f231e
Mendel Liang <mendel.liang@live.com.au>
2015-09-12 11:13:02 +1000

    Changed some stuff to contsants

--- public/client.js
+++ public/client.js
@@ -20,7 +20,7 @@ $(function() {
     var username;
     var connected = false;
 
-    var adminName = "a";
+    var ADMIN_NAME = "a";
 
     var socket = io();
     
@@ -115,10 +115,10 @@ $(function() {
     socket.on('change status', function(status){
         myStatus = status;
         var text = "";
-        if(myStatus == "surv"){
+        if(myStatus == "SURV"){
             text = "Survivor";
         }
-        else if(myStatus == "zomb"){
+        else if(myStatus == "ZOMB"){
             text = "Zombie";
         }
         else{
@@ -162,7 +162,7 @@ $(function() {
         }
     }, 1000);
     function showPage(){
-        if(username != adminName){
+        if(username != ADMIN_NAME){
             $(adminPanel).hide();
             $(playerPanel).show();
         }
